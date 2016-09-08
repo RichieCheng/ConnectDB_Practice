b5b5506fce18189cd484ffd21b84a1e91832f842
ChengRichie <RichieRich@richierichs-macbook.local>
2016-09-08 18:25:01 +0800

    Initial Commit

---
+++ ConnectDB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConnectDB
+//
+//  Created by ChengRichie on 2016/9/8.
+//  Copyright © 2016年 RichieRich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
